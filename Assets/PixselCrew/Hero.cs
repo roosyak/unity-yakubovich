f8ee0b659f9f49a94754c5a74375df520aad3647
Руслан <git@yaruslan.ru>
2022-09-06 22:07:40 +0300

    создал скрипты

---
+++ Assets/PixselCrew/Hero.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private float _direction;
+    
+    public void SetDirection(float direction) {
+        _direction = direction;
+    }
+    private void Update()
+    {
+        if (_direction != 0) {
+            var delta = _direction * _speed * Time.deltaTime;
+            var x = transform.position.x + delta;
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+        }
+    }
+    public void SaySomething() {
+        Debug.Log("SaySomething");
+    }
+}
---
+++ Assets/PixselCrew/HeroInputReader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HeroInputReader : MonoBehaviour
+{
+   [SerializeField] private Hero _hero;
+
+    private void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        _hero.SetDirection(h);
+
+        if (Input.GetButtonUp("Fire1")) {
+            _hero.SaySomething();
+        }
+    }
+}
