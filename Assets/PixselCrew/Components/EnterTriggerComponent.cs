b720385009de7564c300648d1b51bf71c5395ccb
Руслан <git@yaruslan.ru>
2022-09-11 20:20:53 +0300

    перезагрузка сцены по триггеру

---
+++ Assets/PixselCrew/Components/EnterTriggerComponent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace PixelCrew.Components
+{
+    public class EnterTriggerComponent : MonoBehaviour
+    {
+        /// <summary>
+        /// тег с которым срабатывает событие 
+        /// </summary>
+        [SerializeField] private string _tag;
+
+        /// <summary>
+        /// внешний метод который нужно выполнить
+        /// </summary>
+        [SerializeField] private UnityEvent _action;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag(_tag))
+            {
+                _action?.Invoke();
+            }
+        }
+    }
+}
+
---
+++ Assets/PixselCrew/Components/ReloadLavelComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace PixelCrew.Components
+{
+    public class ReloadLavelComponent : MonoBehaviour
+    {
+        public void Reload()
+        {
+            var scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
