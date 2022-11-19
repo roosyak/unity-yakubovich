5dd5d51fde3bb6210bbbbc1107d91634f956bd63
Руслан <git@yaruslan.ru>
2022-11-19 20:17:59 +0300

    движение монеток

---
+++ Assets/PixselCrew/Components/Movement/VerticalLivetationComponent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace PixselCrew.Components
+{
+    public class VerticalLivetationComponent : MonoBehaviour
+    {
+        [SerializeField] private float _frequency = 1f; // длинна «волны»
+        [SerializeField] private float _amplitude = 1f; // амплитуда
+        [SerializeField] private bool _randomize;
+        private float _originalY;
+        private Rigidbody2D _rigidbody;
+
+        private float _seed;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _originalY = _rigidbody.position.y;
+            if (_randomize)
+                _seed = Random.value * Mathf.PI * 2;
+        }
+        private void Update()
+        {
+            var pos = _rigidbody.position;
+            pos.y = _originalY + Mathf.Sin(_seed + Time.time * _frequency) * _amplitude;
+            _rigidbody.MovePosition(pos);
+        }
+    }
+}
