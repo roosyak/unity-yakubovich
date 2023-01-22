983e2335cbb034c8feb9c29855ab808d546276d6
Руслан <git@yaruslan.ru>
2023-01-22 13:42:30 +0300

    применение настроек звука к сцене

---
+++ Assets/PixselCrew/Components/Audio/AudioSettingsComponent.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using PixselCrew.Model;
+using System;
+
+namespace PixselCrew.Components.Audio
+{
+    /*
+     применение настроек к звукам на сцене 
+     */
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioSettingsComponent : MonoBehaviour
+    {
+        [SerializeField] private SoundSetting _mode;
+        private AudioSource _source;
+        private FloatPersistenProperty _model;
+        private void Start()
+        {
+            _source = GetComponent<AudioSource>();
+            _model = FindProperty();
+            _model.OnChanged += OnSoundSettingsChanged;
+            OnSoundSettingsChanged(_model.Value, _model.Value);
+        }
+
+        private void OnSoundSettingsChanged(float newValue, float oldValue)
+        {
+            _source.volume = newValue;
+        }
+
+        private FloatPersistenProperty FindProperty()
+        {
+            switch (_mode)
+            {
+                case SoundSetting.Music:
+                    return GameSettings.I.Music;
+                case SoundSetting.Sfx:
+                    return GameSettings.I.Sfx;
+            }
+
+            throw new ArgumentException("Undefined mode");
+
+        }
+
+        private void OnDestroy()
+        {
+            _model.OnChanged -= OnSoundSettingsChanged;
+        }
+    }
+}
---
+++ Assets/PixselCrew/UI/Widgets/ButtonSound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.PixselCrew.UI.Widgets
+{
+    /*
+     проиграть звук по нажатию кнопки
+     */
+    public class ButtonSound : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private AudioClip _audioClip;
+        private AudioSource _source;
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (_source == null)
+                // ищем один раз
+                _source = GameObject.FindWithTag("SfxAudioSource").GetComponent<AudioSource>();
+            // проиграть один раз
+            _source.PlayOneShot(_audioClip);
+        }
+    }
+}
