98d094303f69ecb6fd75250d70d8f37f5dcd63c5
Руслан <git@yaruslan.ru>
2023-02-19 12:39:35 +0300

    шаблон для инвенторя

---
+++ Assets/PixselCrew/UI/Hud/QuickInventory/QuickInventoryController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine; 
+using PixselCrew.Utils.Disposables;
+using PixselCrew.Model;
+using System;
+
+namespace PixselCrew.UI
+{
+    public class QuickInventoryController : MonoBehaviour
+    {
+        [SerializeField] private Transform _conteiner;
+        [SerializeField] private GameObject _prefab;
+        private readonly CompositeDisposable _trash = new CompositeDisposable();
+
+        private GameSession _session;
+
+        private void Start()
+        {
+            _session = FindObjectOfType<GameSession>();
+            // subscribe model 
+            Rebuild();
+        }
+
+        private void Rebuild()
+        { 
+
+        }
+    }
+}
